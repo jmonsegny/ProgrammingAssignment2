62175adac6313c2ca6e6b9c86090435c3f37d3c1
jmonsegny <jmonsegny@hotmail.com>
2016-05-20 23:01:16 -0500

    Initial comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Jorge Monsegny 2016
+## This is the second assignment of the course R
+## Programming from the JHU-Coursera Data Science
+## Specialization.
+## The objective is to write two functions that 
+## use R lexical scoping to avoid the matrix inverse
+## calculation by returning a cached result whenever 
+## is possible. 
 
-## Write a short comment describing this function
+## This function receives a matrix and returns
+## a list with setter and getter functions for
+## the matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This function receives a list returned by
+## the makeCacheMatrix function and returns
+## the inverse of the matrix only is it was
+## not previously calculated.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
